b20b066f90bc452564f4cb9314e2517ea0cdd5b6
neXus <gamingmaster452005@gmail.com>
2026-03-29 12:37:31 +0530

    28th March

---
+++ discount_calculator.py
@@ -0,0 +1,18 @@
+price=input("Enter price:")
+discount=input("Enter discount:")
+
+
+def apply_discount(price,discount):
+    if not isinstance(price,(int,float)):
+        return("The price should be a number")
+    if not isinstance(discount,(int,float)):
+        return("The discount should be a number")
+    if price<=0:
+        return("The price should be greater than 0")
+    if discount<0 or discount>100:
+        return("The discount should be between 0 and 100")
+
+final_price=float(price)-float(price)*(float(discount)/100)
+print(final_price)
+
+apply_discount(price,discount)
